c0e6bedd986a5c6655d1dc8a6da63d20a32c1db2
raphael_baron <raphael.baron@liksi.fr>
2021-09-09 15:51:52 +0200

    Correction TP_4_2

--- src/app/component/header/header.component.ts
+++ src/app/component/header/header.component.ts
@@ -1,22 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { UserService } from "../../service/user/user.service";
 import { User } from "../../model/user.model";
+import { Subscription } from "rxjs";
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
   currentUser?: User;
+
+  subscription?: Subscription;
 
   constructor(private userService: UserService) {
   }
 
   ngOnInit(): void {
-    this.userService.user.subscribe(
+    this.subscription = this.userService.user.subscribe(
       user => this.currentUser = user,
       error => console.log(error)
     )
+  }
+
+  ngOnDestroy(): void {
+    this.subscription?.unsubscribe();
   }
 }
